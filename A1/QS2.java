1d271fce7343215fd5cd0e3ae544810b6ede6238
Arb4n11 <129899659+Arb4n11@users.noreply.github.com>
2023-04-15 01:46:54 +0200

    TASK 4 and 5

---
+++ A1/QS2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+// This class defines a simple queuing system with one server. It inherits Proc so that we can use time and the
+// signal names without dot notation
+class QS2 extends Proc{
+	public int numberInQueue = 0, accumulated, noMeasurements;
+	public Proc sendTo;
+	Random slump = new Random();
+
+	public void TreatSignal(Signal x){
+		switch (x.signalType){
+
+			case ARRIVAL:{
+				numberInQueue++;
+				if (numberInQueue == 1){
+					SignalList.SendSignal(READY,this, time + Math.abs(4 * Math.log(1 - slump.nextDouble()))); //service time with
+				}
+			} break;
+
+			case READY:{
+				numberInQueue--;
+				if (sendTo != null){
+					SignalList.SendSignal(ARRIVAL, sendTo, time);
+				}
+				if (numberInQueue > 0){
+					SignalList.SendSignal(READY, this, time + Math.abs(4 * Math.log(1 - slump.nextDouble()))); //time + 0.2*slump.nextDouble()
+				}
+			} break;
+
+			case MEASURE:{
+				noMeasurements++;
+				accumulated = accumulated + numberInQueue;
+				SignalList.SendSignal(MEASURE, this, time + Math.abs(4 * Math.log(1 - slump.nextDouble())));
+			} break;
+		}
+	}
+}
