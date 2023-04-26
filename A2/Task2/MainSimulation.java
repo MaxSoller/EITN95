659363183c181b8b867c80b5beba51d13f979d00
Sebastian <sebastian.malmstrom@hotmail.se>
2023-04-26 11:12:05 +0200

    small cleanup

--- A2/Task2/MainSimulation.java
+++ A2/Task2/MainSimulation.java
@@ -44,16 +44,15 @@ public class MainSimulation extends GlobalSimulation{
 		double endTimeAverage = averageDouble(endTimes);
 
 		double standardDeviation = standardDev(queueTimess);
+		System.out.println(timeAverage);
 		double low = timeAverage - 1.96*standardDeviation;
 		double high = timeAverage + 1.96*standardDeviation;
 		System.out.println("Average time of a prescription fill: (95% confidence interval) (Low) " + low + " - (High) " + high);
 		
 		double standardDevationEndTime = standardDev(endTimes);
-
+		System.out.println(endTimeAverage);
 		double lowEndTime = endTimeAverage - 1.96*standardDevationEndTime;
 		double highEndTime = endTimeAverage + 1.96*standardDevationEndTime;
-		DecimalFormat numberFormat = new DecimalFormat("00");
-		DecimalFormat numberFormat2 = new DecimalFormat("0");
 
 		System.out.println("Average end time of day: (95% confidence interval) (Low) " +  (lowEndTime) + " - (High) " + (highEndTime));
 
@@ -64,15 +63,14 @@ public class MainSimulation extends GlobalSimulation{
 		for(double i : values){
 			sum = sum + i;
 		}
-		
-		double average = sum/values.size();
+		int size = values.size();
+		double average = sum/size;
 
 		double variance = 0.0;
-		for(int i = 0; i < values.size(); i++){
-			variance = variance + (Math.pow((values.get(i)-average),2));
+		for(double num : values){
+			variance = variance + (Math.pow(num-average,2));
 		}
-		variance = variance/values.size();
-		return Math.sqrt(variance);
+		return Math.sqrt(variance/size);
 	}
 
 	public static Double averageDouble(List<Double> values){
